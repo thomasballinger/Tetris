049b2618a99cbc9668945252c8dcd0a1dac75c4c
Thomas Ballinger <thomasballinger@gmail.com>
2013-04-10 21:57:39 -0400

    add placeholder server

---
+++ jsonserver.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+import json
+
+scores = []
+
+def score_sort(score):
+    return score.get('score', -1)
+
+@app.route("/", methods=["GET", "POST"])
+def hello():
+    if request.method == 'POST':
+        print repr(request.data)
+        print repr(request.form.keys()[0])
+        scores.append(json.loads(request.data or request.form.keys()[0]))
+        scores.sort(key=score_sort)
+        return 'OK\n'
+    else:
+        return json.dumps(scores)
+
+@app.route("/reset")
+def reset():
+    global scores
+    scores = set()
+
+if __name__ == "__main__":
+    app.run(port=12345, host='', debug=True)
